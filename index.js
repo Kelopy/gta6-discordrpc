987ebcdf3635cf8679aa2ad884efcfa6e38bdd07
Kelopy <theytrank@gmail.com>
2022-09-21 21:19:46 +0100

    prettier

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const RPC = require("discord-rpc");
-const client = new RPC.Client({transport: "ipc"});
-const { clientid } = require('./config.json');
+const client = new RPC.Client({ transport: "ipc" });
+const { clientid } = require("./config.json");
 
 const activity = {
-    details: "chillin' in vice city",
-    assets: {
-        large_image: "gta-6-icon",
-        large_text: "Grand Theft Auto VI"
-    },
-    // buttons: [
-    //     {
-    //         "label":"google",
-    //         "url":"https://google.com/"
-    //     }
-    // ],
-    timestamps: {start: Date.now()},
-    instance: true
+  details: "chillin' in vice city",
+  assets: {
+    large_image: "gta-6-icon",
+    large_text: "Grand Theft Auto VI",
+  },
+  // buttons: [
+  //     {
+  //         "label":"google",
+  //         "url":"https://google.com/"
+  //     }
+  // ],
+  timestamps: { start: Date.now() },
+  instance: true,
 };
 
 client.on("ready", () => {
-    client.request("SET_ACTIVITY", {pid: process.pid, activity: activity});
-    console.log("You're now playing Grand Theft Auto VI");
-})
+  client.request("SET_ACTIVITY", { pid: process.pid, activity: activity });
+  console.log("You're now playing Grand Theft Auto VI");
+});
 
 client.login({ clientId: clientid });
